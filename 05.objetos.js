d4d56708e4a415d23f17626477be29e00f8f6b67
Arthur-Winnicios <arthurwinniciosarruda@gmail.com>
2024-09-09 11:30:48 -0400

    add poo objeto 2

--- 05.objetos.js
+++ 05.objetos.js
@@ -12,8 +12,22 @@ class Pessoa {
   falar() {
     console.log(`Ola, meu nome e ${this.nome}!`);
   }
+  falar() {
+    console.log(`minha idade é  ${this.idade}!`);
+  }
 }
 
-// Objetao/Instancia "!pessoal" criada apartir da classe Pessoa 
-const pessoal = new Pessoa('Ana Luiza', 16, 1.72, 62);
+// Objeto/Instancia "!pessoal" criada a partir da classe Pessoa 
+const pessoa1 = new Pessoa('Ana Luiza', 16, 1.72, 62);
 pessoal.falar(); // Chamada do metodo falara() da Classe Pessoa 
+pessoa1.falarIdade(); //
+
+
+const pessoa2 = new Pessoa('Allan', 22, 1.85, 78);
+pessoa2.falar(); // Chamada do metodo falara() da Classe Pessoa
+pessoa2.falarIdade(); // Chamada do metodo falara() da
+
+
+const pessoa3 = new Pessoa('Allan', 22, 1.85, 78);
+pessoa3.falar(); // Chamada do metodo falara() da Classe Pessoa
+pessoa3.falarIdade();
