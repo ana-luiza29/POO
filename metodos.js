3bae3d202454ac579b5939543beb4dca7dd86e32
Arthur-Winnicios <arthurwinniciosarruda@gmail.com>
2024-09-09 08:57:34 -0400

    POO metodos.js

---
+++ metodos.js
@@ -0,0 +1,15 @@
+class Pessoa {
+  constructor(nome, idade, altura, peso, genero) {
+    // atributos
+    // inicializaçao do atri
+    this.altura = altura;
+    this.peso = peso;
+    this.nome = nome;
+    this.idade = idade;
+    this.genero = genero;
+  }
+  // metodos 
+  falar() {
+    console.log(`Ola, meu nome e ${this.nome}!`);
+  }
+}
