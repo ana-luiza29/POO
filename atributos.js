6ca005660dfc84c238736a47d7dfe76a8fff3fef
Arthur-Winnicios <arthurwinniciosarruda@gmail.com>
2024-09-09 08:43:34 -0400

    POO atributos.js

---
+++ atributos.js
@@ -0,0 +1,9 @@
+class Pessoa {
+    constructor(nome, idade, altura, peso) { // atributos 
+        // inicializaçao do atri
+        this.altura = altura;
+        this.peso = peso;
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
