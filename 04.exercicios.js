30d08f4f69466ce0569d67aa2f4b0faa44f1208e
Arthur-Winnicios <arthurwinniciosarruda@gmail.com>
2024-09-09 10:16:50 -0400

    POO exercicios.js

---
+++ 04.exercicios.js
@@ -0,0 +1,19 @@
+class Marcas {
+  constructor(nike, gucci, versace, chanel, prada) {
+    this.nike = nike;
+    this.gucci = gucci;
+    this.versace = versace;
+    this.chanel = chanel;
+    this.prada = prada;
+  }
+  // metodos
+   roupa() {
+    console.log(`roupas que irei utilizar ${this.gucci}!`);
+  }
+  tenis() {
+    console.log(` meu tenis favorito ${this.nike}!`);
+  }
+  bolsa() {
+    console.log(` minha bolsa dos sonhos ${this.chanel}!`);
+  } 
+    }
