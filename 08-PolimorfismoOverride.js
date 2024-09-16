98e7abe1e1d5eceef7635b38c2ca90b83cdfa27b
Arthur-Winnicios <arthurwinniciosarruda@gmail.com>
2024-09-16 08:08:54 -0400

    add class guerreiro

--- 08-PolimorfismoOverride.js
+++ 08-PolimorfismoOverride.js
@@ -18,3 +18,17 @@ atacar() {
     console.log(`${this.nome} recebeu ${dano} de dano. Vida restante: ${this.vida}`);
  }
 }
+
+// Classe  Guerreiro que herda de personagem
+
+class Guerreiro extends Personagem {
+    constructor(nome, vida, arma) {
+        super(nome, vida);
+        this.arma = arma;
+    }
+
+    atacar() {
+        super.atacar();
+        console.log(`${this.nome} realiza um ataque com a ${this.arma}`);
+    }
+}
