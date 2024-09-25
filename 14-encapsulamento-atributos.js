4240685ba1b9c8fcad144ddc67e792e4f20b398b
Arthur-Winnicios <arthurwinniciosarruda@gmail.com>
2024-09-25 07:45:23 -0400

    encapsulamento de atributos.js

---
+++ 14-encapsulamento-atributos.js
@@ -0,0 +1,16 @@
+
+class Personagem {
+  #nome;
+  #classes;
+  #nivel;
+  #vida;
+  #mana;
+
+  constructor(nome, classes, vida, mana) {
+    this.#nome = nome;
+    this.#classes = classes;
+    this.#nivel = nivel;
+    this.#vida = vida;
+    this.#mana = mana;
+  }
+}
